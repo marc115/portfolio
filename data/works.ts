945cbb1b16a34ea5e8d4e977303c44a4d0e35e0e
Juan Marcano <marchanoth@protonmail.com>
2024-08-28 12:33:47 -0500

    fixing works

--- data/works.ts
+++ data/works.ts
@@ -5,10 +5,10 @@ export const works = [
         thumbnail: '/works/nike-lookalike-thumbnail.png',
         liveUrl: 'https://nike-lookalike.vercel.app/'
     },
-    {
-        name: 'Nike Lookalike',
-        description: 'A mock website inspired by Nike to test out my Tailwind skills',
-        thumbnail: '',
-        // liveUrl: 'https://nike-lookalike.vercel.app/'
-    }
+    // {
+    //     name: 'Nike Lookalike',
+    //     description: 'A mock website inspired by Nike to test out my Tailwind skills',
+    //     thumbnail: '',
+    //     // liveUrl: 'https://nike-lookalike.vercel.app/'
+    // }
 ]
