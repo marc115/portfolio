a3fff926937bbcf458734691ff21a6ce10b2abaf
Juan Marcano <marchanoth@protonmail.com>
2024-09-03 17:41:41 -0500

    about me description

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -4,7 +4,8 @@ export default function Me () {
             <h1 className="font-bold text-5xl">I'm
             <span className="gradient-text bg-gradient-to-r from-slate-900 to-sky-800 dark:from-slate-50 dark:to-sky-300 bg-clip-text text-transparent"> Juan</span>, 
             a software developer aiming to build practical software.</h1>
-            <p></p>
+            <p>A professional that learned to code both from a school and on my own, I don't shy away from searching online when the need calls for it.</p>
+            <p>I graduated as a software developer in Peru at the end of 2023, and have started to work shortly since then. </p>
         </div>
     )
 }
