def9779a04a3d3ffd74922a7cf3b3c78a5b9e521
Juan Marcano <marchanoth@protonmail.com>
2024-09-03 19:46:10 -0500

    trying to fix apostrophe

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,10 +1,10 @@
 export default function Me () {
     return (
         <div className="m-5 md:mx-32">
-            <h1 className="font-bold text-5xl">I'm
+            <h1 className="font-bold text-5xl">I&aposm
             <span className="gradient-text bg-gradient-to-r from-slate-900 to-sky-800 dark:from-slate-50 dark:to-sky-300 bg-clip-text text-transparent"> Juan</span>, 
             a software developer aiming to build practical software.</h1>
-            <p>A professional that learned to code both from a school and on my own, I don't shy away from searching online when the need calls for it.</p>
+            <p>A professional that learned to code both from a school and on my own, I don&apost shy away from searching online when the need calls for it.</p>
             <p>I graduated as a software developer in Peru at the end of 2023, and have started to work shortly since then. </p>
         </div>
     )
